157f3247df77f2730b11042a5c232c9f32a0ee8a
You have one new message <gabriel_pirastru@hotmail.fr>
2026-03-25 12:18:10 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
