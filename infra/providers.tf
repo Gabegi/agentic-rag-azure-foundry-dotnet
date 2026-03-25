e343f79fac287a36c34d2584057c0e6dde7dce6a
You have one new message <gabriel_pirastru@hotmail.fr>
2026-03-25 12:18:09 +0100

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,28 @@
+# providers.tf
+# Terraform and provider version constraints
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "bestnamesa"
+    container_name       = "agent-rag-tfstate"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
