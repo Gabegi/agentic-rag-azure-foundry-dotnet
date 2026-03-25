aa3c064deb947e213fe708de8b565dc61eb2ba3b
You have one new message <gabriel_pirastru@hotmail.fr>
2026-03-25 12:24:22 +0100

    ai foundry

---
+++ infra/ai_foundry.tf
@@ -0,0 +1,29 @@
+resource "azurerm_storage_account" "main" {
+  name                     = "stsupportagentdev"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    project     = "support-agent"
+    environment = "dev"
+  }
+}
+
+resource "azurerm_ai_foundry" "main" {
+  name                = "aif-support-agent-dev"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  storage_account_id  = azurerm_storage_account.main.id
+  key_vault_id        = azurerm_key_vault.main.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    project     = "support-agent"
+    environment = "dev"
+  }
+}
