012232c513d195c7ac509a90da21ac9aed3d42ec
You have one new message <george.trombley6000@gmail.com>
2026-04-07 12:41:41 +0200

    Create storage.tf

---
+++ infra/storage.tf
@@ -0,0 +1,21 @@
+# storage.tf
+# Storage account and blob container for RAG document ingestion
+
+resource "azurerm_storage_account" "documents" {
+  name                     = "stragdocsdev"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    project     = "support-agent"
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "documents" {
+  name                  = "documents"
+  storage_account_id    = azurerm_storage_account.documents.id
+  container_access_type = "private"
+}
