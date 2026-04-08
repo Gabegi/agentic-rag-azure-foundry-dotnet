fcd58dffc92687df23d9d100f9c3a43eb64dba35
You have one new message <george.trombley6000@gmail.com>
2026-04-08 12:27:39 +0200

    Create Program.cs

---
+++ src/InvoiceIndexer/Program.cs
@@ -0,0 +1,52 @@
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using InvoiceIndexer.Configuration;
+using InvoiceIndexer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(log =>
+    {
+        log.ClearProviders();
+        log.AddConsole();
+        log.SetMinimumLevel(LogLevel.Information);
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        var config = new IndexerConfig
+        {
+            SearchEndpoint               = ctx.Configuration["SEARCH_ENDPOINT"]!,
+            OpenAiEndpoint               = ctx.Configuration["OPENAI_ENDPOINT"]!,
+            OpenAiEmbeddingDeployment    = ctx.Configuration["OPENAI_EMBEDDING_DEPLOYMENT"]!,
+            OpenAiGptDeployment          = ctx.Configuration["OPENAI_GPT_DEPLOYMENT"]!,
+            StorageAccountUrl            = ctx.Configuration["STORAGE_ACCOUNT_URL"]!,
+            StorageContainer             = ctx.Configuration["STORAGE_CONTAINER"]!,
+            SearchIndexName              = ctx.Configuration["SEARCH_INDEX_NAME"]!,
+            KnowledgeSourceName          = ctx.Configuration["KNOWLEDGE_SOURCE_NAME"]!,
+            KnowledgeBaseName            = ctx.Configuration["KNOWLEDGE_BASE_NAME"]!,
+            DocumentIntelligenceEndpoint = ctx.Configuration["DOCUMENT_INTELLIGENCE_ENDPOINT"]!,
+        };
+
+        var credential = new DefaultAzureCredential();
+
+        services.AddSingleton(config);
+        services.AddSingleton(credential);
+
+        // Azure clients
+        services.AddSingleton(_ =>
+            new BlobServiceClient(new Uri(config.StorageAccountUrl), credential));
+
+        services.AddSingleton(_ =>
+            new DocumentAnalysisClient(
+                new Uri(config.DocumentIntelligenceEndpoint), credential));
+
+        // Services
+        services.AddSingleton<IBlobService, BlobService>();
+        services.AddSingleton<IPdfExtractor, PdfExtractorService>();
+    })
+    .Build();
+
+await host.RunAsync();
