ff1f299d339942b60b10977324a936712976d6d3
You have one new message <george.trombley6000@gmail.com>
2026-04-08 11:32:36 +0200

    index config and invoice chunk models

---
+++ src/InvoiceIndexer/Models/InvoiceChunk.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace InvoiceIndexer.Models;
+
+public class InvoiceChunk
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = default!;
+
+    [JsonPropertyName("page_number")]
+    public int PageNumber { get; set; }
+
+    [JsonPropertyName("page_chunk")]
+    public string PageChunk { get; set; } = default!;
+
+    [JsonPropertyName("page_embedding_text_3_large")]
+    public IReadOnlyList<float> PageEmbeddingText3Large { get; set; } = default!;
+}
