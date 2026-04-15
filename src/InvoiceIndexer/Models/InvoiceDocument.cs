4ede0a798d3d791390f6c4b99d9812e9552e6ea4
You have one new message <george.trombley6000@gmail.com>
2026-04-15 15:03:11 +0200

    Update InvoiceDocument.cs

--- src/InvoiceIndexer/Models/InvoiceDocument.cs
+++ src/InvoiceIndexer/Models/InvoiceDocument.cs
@@ -5,10 +5,10 @@ namespace InvoiceIndexer.Models;
 public class InvoiceDocument
 {
     [JsonPropertyName("id")]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [JsonPropertyName("vendor")]
-    public string Vendor { get; set; }
+    public string? Vendor { get; set; }
 
     [JsonPropertyName("amount")]
     public double? Amount { get; set; }
@@ -17,20 +17,20 @@ public class InvoiceDocument
     public double? Discount { get; set; }
 
     [JsonPropertyName("category")]
-    public string Category { get; set; }
+    public string? Category { get; set; }
 
     [JsonPropertyName("date")]
     public DateTimeOffset? Date { get; set; }
 
     [JsonPropertyName("payment_terms")]
-    public string PaymentTerms { get; set; }
+    public string? PaymentTerms { get; set; }
 
     [JsonPropertyName("source_file")]
-    public string SourceFile { get; set; }
+    public string? SourceFile { get; set; }
 
     [JsonPropertyName("content")]
-    public string Content { get; set; }
+    public string? Content { get; set; }
 
     [JsonPropertyName("content_vector")]
-    public float[] ContentVector { get; set; }
+    public float[]? ContentVector { get; set; }
 }
