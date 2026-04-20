366e7b01395a85a40adf69a4af501e5c772c06b1
You have one new message <george.trombley6000@gmail.com>
2026-04-20 13:30:04 +0200

    Create act.tf

---
+++ infra/act.tf
@@ -0,0 +1,19 @@
+resource "azurerm_container_registry" "main" {
+  name                = "crragtest"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false # Use managed identity, not admin credentials
+
+  tags = {
+    project     = "support-agent"
+    environment = "dev"
+  }
+}
+
+# Allow ACI to pull images from ACR
+resource "azurerm_role_assignment" "mi_acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = data.azurerm_virtual_machine.runner.identity[0].principal_id
+}
