0e3f70b46c6fbc9bd0ef58ddc6db4d7fb11a502f
You have one new message <george.trombley6000@gmail.com>
2026-04-21 11:23:00 +0200

    Create outputs.tf

---
+++ infra/outputs.tf
@@ -0,0 +1,29 @@
+output "search_endpoint" {
+  value       = "https://${azurerm_search_service.main.name}.search.windows.net"
+  description = "SEARCH_ENDPOINT for InvoiceIndexer"
+}
+
+output "openai_endpoint" {
+  value       = azurerm_cognitive_account.openai.endpoint
+  description = "OPENAI_ENDPOINT for InvoiceIndexer"
+}
+
+output "document_intelligence_endpoint" {
+  value       = azurerm_cognitive_account.document_intelligence.endpoint
+  description = "DOCUMENT_INTELLIGENCE_ENDPOINT for InvoiceIndexer"
+}
+
+output "storage_account_url" {
+  value       = azurerm_storage_account.documents.primary_blob_endpoint
+  description = "STORAGE_ACCOUNT_URL for InvoiceIndexer"
+}
+
+output "storage_container" {
+  value       = azurerm_storage_container.documents.name
+  description = "STORAGE_CONTAINER for InvoiceIndexer"
+}
+
+output "container_registry_login_server" {
+  value       = azurerm_container_registry.main.login_server
+  description = "ACR login server for pushing/pulling images"
+}
