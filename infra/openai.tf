d2d163105fc46696d442056e57046b0a5af2d6cb
You have one new message <george.trombley6000@gmail.com>
2026-04-21 11:21:39 +0200

    Azure services

---
+++ infra/openai.tf
@@ -0,0 +1,50 @@
+resource "azurerm_cognitive_account" "openai" {
+  name                = "oai-support-agent-dev"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  kind                = "OpenAI"
+  sku_name            = "S0"
+
+  tags = {
+    project     = "support-agent"
+    environment = "dev"
+  }
+}
+
+resource "azurerm_cognitive_deployment" "embedding" {
+  name                 = var.openai_embedding_deployment
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+
+  model {
+    format  = "OpenAI"
+    name    = "text-embedding-3-large"
+    version = "1"
+  }
+
+  sku {
+    name     = "Standard"
+    capacity = 50
+  }
+}
+
+resource "azurerm_cognitive_deployment" "querying" {
+  name                 = var.openai_gpt_deployment
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+
+  model {
+    format  = "OpenAI"
+    name    = var.openai_gpt_model_name
+    version = "2024-07-18"
+  }
+
+  sku {
+    name     = "Standard"
+    capacity = 10
+  }
+}
+
+resource "azurerm_role_assignment" "sp_openai_user" {
+  scope                = azurerm_cognitive_account.openai.id
+  role_definition_name = "Cognitive Services OpenAI User"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,21 @@ variable "subscription_id" {
   description = "Azure subscription ID to deploy resources into"
   type        = string
 }
+
+variable "openai_embedding_deployment" {
+  description = "Name of the Azure OpenAI embedding deployment"
+  type        = string
+  default     = "text-embedding-3-large"
+}
+
+variable "openai_gpt_deployment" {
+  description = "Name of the Azure OpenAI GPT deployment"
+  type        = string
+  default     = "gpt-4o-mini"
+}
+
+variable "openai_gpt_model_name" {
+  description = "Model name for the Azure OpenAI GPT deployment"
+  type        = string
+  default     = "gpt-4o-mini"
+}
