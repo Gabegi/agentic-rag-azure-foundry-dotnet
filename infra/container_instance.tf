7de0a3bf1e907babf71dd07dd956d25a0f4e0213
You have one new message <george.trombley6000@gmail.com>
2026-04-21 12:25:42 +0200

    indexer app

--- infra/container_instance.tf
+++ infra/container_instance.tf
@@ -64,7 +64,7 @@ resource "azurerm_container_group" "invoice_indexer" {
 
   container {
     name   = "invoice-indexer"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
+    image  = "${azurerm_container_registry.main.login_server}/invoice-indexer:${var.indexer_image_tag}"
     cpu    = "1"
     memory = "2"
 
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -27,3 +27,8 @@ output "container_registry_login_server" {
   value       = azurerm_container_registry.main.login_server
   description = "ACR login server for pushing/pulling images"
 }
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.main.name
+  description = "Name of the main resource group"
+}
