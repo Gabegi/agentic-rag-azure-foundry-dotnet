f650a98653adb675622b2d51f10a3540346130fb
You have one new message <george.trombley6000@gmail.com>
2026-04-21 11:58:39 +0200

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -20,8 +20,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id                  = var.subscription_id
-  resource_provider_registrations = "none"
+  subscription_id = var.subscription_id
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
