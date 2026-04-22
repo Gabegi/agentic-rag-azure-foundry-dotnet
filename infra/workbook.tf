bf353b8c71109279d5f2ae2f5555453d668e4673
You have one new message <george.trombley6000@gmail.com>
2026-04-22 15:03:25 +0200

    workbook

--- infra/workbook.tf
+++ infra/workbook.tf
@@ -1,5 +1,7 @@
+resource "random_uuid" "workbook" {}
+
 resource "azurerm_application_insights_workbook" "main" {
-  name                = "workbook-support-agent-dev"
+  name                = random_uuid.workbook.result
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   display_name        = "Invoice Indexer — Observability"
